6014a420a31896adb65cb60799cdb9bd6078fea7
Dmitriy Monakhov <dimamon6032403@gmail.com>
2025-07-07 17:15:45 +0200

    WriteRead added

--- public/PiSubmarine/Api/Internal/SPI/DriverConcept.h
+++ public/PiSubmarine/Api/Internal/SPI/DriverConcept.h
@@ -1,16 +1,14 @@
 #pragma once
 
+#include <cstdint>
 #include "PiSubmarine/Api/Internal/SPI/Callback.h"
 
 namespace PiSubmarine::Api::Internal::SPI
 {
 	template<typename T>
-	concept DriverConcept = requires(T driver, uint8_t * txData, uint8_t * rxData, size_t len, Callback callback)
+	concept DriverConcept = requires(T driver, uint8_t* txData, uint8_t* rxData, size_t len)
 	{
-		{ driver.Write(txData, len) } -> std::same_as<bool>;
-		{ driver.Read(rxData, len) } -> std::same_as<bool>;
-		{ driver.WriteAsync(txData, len, callback) } -> std::same_as<bool>;
-		{ driver.ReadAsync(rxData, len, callback) } -> std::same_as<bool>;
+		{ driver.WriteRead(txData, rxData, len) } -> std::same_as<bool>;
 	};
 
 }
